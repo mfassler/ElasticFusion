b47681e185f5e0bfc6c2a25df0dbae2caab83681
Mark Fassler <mfassler@users.noreply.github.com>
2018-04-11 17:28:15 +0900

    changes to make work with Intel librealsense version 2.  Works for Me!(tm).

--- GUI/src/Tools/RealSenseInterface.cpp
+++ GUI/src/Tools/RealSenseInterface.cpp
@@ -1,24 +1,24 @@
 #include "RealSenseInterface.h"
 #include <functional>
 
+#include <stdio.h>
+
 #ifdef WITH_REALSENSE
 RealSenseInterface::RealSenseInterface(int inWidth,int inHeight,int inFps)
   : width(inWidth),
   height(inHeight),
   fps(inFps),
-  dev(nullptr),
   initSuccessful(true)
 {
-  if(ctx.get_device_count() == 0)
+  auto list = ctx.query_devices();
+  if(list.size() == 0)
   {
     errorText = "No device connected.";
     initSuccessful = false;
     return;
   }
 
-  dev = ctx.get_device(0);
-  dev->enable_stream(rs::stream::depth,width,height,rs::format::z16,fps);
-  dev->enable_stream(rs::stream::color,width,height,rs::format::rgb8,fps);
+  dev = list.front();
 
   latestDepthIndex.assign(-1);
   latestRgbIndex.assign(-1);
@@ -39,27 +39,84 @@ RealSenseInterface::RealSenseInterface(int inWidth,int inHeight,int inFps)
   setAutoExposure(true);
   setAutoWhiteBalance(true);
 
-  rgbCallback = new RGBCallback(lastRgbTime,
-    latestRgbIndex,
-    rgbBuffers);
 
-  depthCallback = new DepthCallback(lastDepthTime,
-    latestDepthIndex,
-    latestRgbIndex,
-    rgbBuffers,
-    frameBuffers);
+  rs2::config cfg;
+  cfg.enable_stream(RS2_STREAM_COLOR, width, height, RS2_FORMAT_RGB8);
+  cfg.enable_stream(RS2_STREAM_DEPTH, width, height);
 
-  dev->set_frame_callback(rs::stream::depth,*depthCallback);
-  dev->set_frame_callback(rs::stream::color,*rgbCallback);
+  pipe.start(cfg);
 
-  dev->start();
+  std::thread t([&]() {
+    while (true)
+    {
+      printf("b4 wait_for_frames()\n");
+      auto frames = pipe.wait_for_frames();
+      printf("after wait_for_frames()\n");
+
+      rs2::video_frame vid_frame = frames.get_color_frame();
+      rs2::depth_frame dep_frame = frames.get_depth_frame();
+      int bufferIndex;
+
+      //  ************************************
+      //   THIS WAS FROM RGBCallback()
+      //  ************************************
+
+      lastRgbTime = std::chrono::duration_cast<std::chrono::milliseconds>(
+        std::chrono::system_clock::now().time_since_epoch()).count();
+
+
+      bufferIndex = (latestRgbIndex.getValue() + 1) % numBuffers;
+
+      memcpy(rgbBuffers[bufferIndex].first, vid_frame.get_data(),
+        vid_frame.get_width() * vid_frame.get_height() * 3);
+
+
+      rgbBuffers[bufferIndex].second = lastRgbTime;
+
+      latestRgbIndex++;
+
+
+      //  ************************************
+      //   THIS WAS FROM DepthCallback()
+      //  ************************************
+      lastDepthTime = std::chrono::duration_cast<std::chrono::milliseconds>(
+        std::chrono::system_clock::now().time_since_epoch()).count();
+
+      bufferIndex = (latestDepthIndex.getValue() + 1) % numBuffers;
+
+      // The multiplication by 2 is here because the depth is actually uint16_t
+      memcpy(frameBuffers[bufferIndex].first.first, dep_frame.get_data(),
+        dep_frame.get_width() * dep_frame.get_height() * 2);
+
+
+      frameBuffers[bufferIndex].second = lastDepthTime;
+
+      int lastImageVal = latestRgbIndex.getValue();
+
+      if(lastImageVal == -1)
+      {
+        return;
+      }
+
+      lastImageVal %= numBuffers;
+
+      memcpy(frameBuffers[bufferIndex].first.second,rgbBuffers[lastImageVal].first,
+        dep_frame.get_width() * dep_frame.get_height() * 3);
+
+      latestDepthIndex++;
+
+    }
+  });
+  t.detach();
+
 }
 
 RealSenseInterface::~RealSenseInterface()
 {
   if(initSuccessful)
   {
-    dev->stop();
+    //dev->stop();
+    pipe.stop();
 
     for(int i = 0; i < numBuffers; i++)
     {
@@ -72,29 +129,46 @@ RealSenseInterface::~RealSenseInterface()
       free(frameBuffers[i].first.second);
     }
 
-    delete rgbCallback;
-    delete depthCallback;
   }
 }
 
 void RealSenseInterface::setAutoExposure(bool value)
 {
-  dev->set_option(rs::option::color_enable_auto_exposure,value);
+  //dev->set_option(rs2::option::color_enable_auto_exposure, value);
+  //auto color_sensor = dev->first<rs2::color_sensor>();
+  auto color_sensor = dev.first<rs2::sensor>();
+  if (color_sensor.supports(RS2_OPTION_ENABLE_AUTO_EXPOSURE)) {
+    color_sensor.set_option(RS2_OPTION_ENABLE_AUTO_EXPOSURE, value);
+  }
 }
 
 void RealSenseInterface::setAutoWhiteBalance(bool value)
 {
-  dev->set_option(rs::option::color_enable_auto_white_balance,value);
+  //dev->set_option(rs2::option::color_enable_auto_white_balance, value);
+  auto color_sensor = dev.first<rs2::sensor>();
+  if (color_sensor.supports(RS2_OPTION_ENABLE_AUTO_WHITE_BALANCE)) {
+    color_sensor.set_option(RS2_OPTION_ENABLE_AUTO_WHITE_BALANCE, value);
+  }
 }
 
 bool RealSenseInterface::getAutoExposure()
 {
-  return dev->get_option(rs::option::color_enable_auto_exposure);
+  //return dev->get_option(rs2::option::color_enable_auto_exposure);
+  auto color_sensor = dev.first<rs2::sensor>();
+  if (color_sensor.supports(RS2_OPTION_ENABLE_AUTO_EXPOSURE)) {
+    return color_sensor.get_option(RS2_OPTION_ENABLE_AUTO_EXPOSURE);
+  }
+  return false;
 }
 
 bool RealSenseInterface::getAutoWhiteBalance()
 {
-  return dev->get_option(rs::option::color_enable_auto_white_balance);
+  //return dev->get_option(rs2::option::color_enable_auto_white_balance);
+  auto color_sensor = dev.first<rs2::sensor>();
+  if (color_sensor.supports(RS2_OPTION_ENABLE_AUTO_WHITE_BALANCE)) {
+    return color_sensor.get_option(RS2_OPTION_ENABLE_AUTO_WHITE_BALANCE);
+  }
+  return false;
 }
 #else
 
--- GUI/src/Tools/RealSenseInterface.h
+++ GUI/src/Tools/RealSenseInterface.h
@@ -4,9 +4,10 @@
 #include <iostream>
 #include <algorithm>
 #include <map>
+#include <cstring>
 
 #ifdef WITH_REALSENSE
-#include "librealsense/rs.hpp"
+#include "librealsense2/rs.hpp"
 #endif
 
 #include "ThreadMutexObject.h"
@@ -18,7 +19,7 @@ public:
   RealSenseInterface(int width = 640,int height = 480,int fps = 30);
   virtual ~RealSenseInterface();
 
-  const int width,height,fps;
+  int width,height,fps;
 
   bool getAutoExposure();
   bool getAutoWhiteBalance();
@@ -34,109 +35,25 @@ public:
   {
     return errorText;
   }
+  std::pair<uint8_t *,int64_t> rgbBuffers[numBuffers];
 
-#ifdef WITH_REALSENSE
-  struct RGBCallback
-  {
-  public:
-    RGBCallback(int64_t & lastRgbTime,
-      ThreadMutexObject<int> & latestRgbIndex,
-      std::pair<uint8_t *,int64_t> * rgbBuffers)
-      : lastRgbTime(lastRgbTime),
-      latestRgbIndex(latestRgbIndex),
-      rgbBuffers(rgbBuffers)
-    {
-    }
-
-    void operator()(rs::frame frame)
-    {
-      lastRgbTime = std::chrono::duration_cast<std::chrono::milliseconds>(
-        std::chrono::system_clock::now().time_since_epoch()).count();
-
-      int bufferIndex = (latestRgbIndex.getValue() + 1) % numBuffers;
-
-      memcpy(rgbBuffers[bufferIndex].first,frame.get_data(),
-        frame.get_width() * frame.get_height() * 3);
-
-      rgbBuffers[bufferIndex].second = lastRgbTime;
-
-      latestRgbIndex++;
-    }
-
-  private:
-    int64_t & lastRgbTime;
-    ThreadMutexObject<int> & latestRgbIndex;
-    std::pair<uint8_t *,int64_t> * rgbBuffers;
-  };
-
-  struct DepthCallback
-  {
-  public:
-    DepthCallback(int64_t & lastDepthTime,
-      ThreadMutexObject<int> & latestDepthIndex,
-      ThreadMutexObject<int> & latestRgbIndex,
-      std::pair<uint8_t *,int64_t> * rgbBuffers,
-      std::pair<std::pair<uint8_t *,uint8_t *>,int64_t> * frameBuffers)
-      : lastDepthTime(lastDepthTime),
-      latestDepthIndex(latestDepthIndex),
-      latestRgbIndex(latestRgbIndex),
-      rgbBuffers(rgbBuffers),
-      frameBuffers(frameBuffers)
-    {
-    }
-
-    void operator()(rs::frame frame)
-    {
-      lastDepthTime = std::chrono::duration_cast<std::chrono::milliseconds>(
-        std::chrono::system_clock::now().time_since_epoch()).count();
-
-      int bufferIndex = (latestDepthIndex.getValue() + 1) % numBuffers;
-
-      // The multiplication by 2 is here because the depth is actually uint16_t
-      memcpy(frameBuffers[bufferIndex].first.first,frame.get_data(),
-        frame.get_width() * frame.get_height() * 2);
-
-      frameBuffers[bufferIndex].second = lastDepthTime;
-
-      int lastImageVal = latestRgbIndex.getValue();
-
-      if(lastImageVal == -1)
-      {
-        return;
-      }
-
-      lastImageVal %= numBuffers;
-
-      memcpy(frameBuffers[bufferIndex].first.second,rgbBuffers[lastImageVal].first,
-        frame.get_width() * frame.get_height() * 3);
-
-      latestDepthIndex++;
-    }
-
-  private:
-    int64_t & lastDepthTime;
-    ThreadMutexObject<int> & latestDepthIndex;
-    ThreadMutexObject<int> & latestRgbIndex;
-
-    std::pair<uint8_t *,int64_t> * rgbBuffers;
-    std::pair<std::pair<uint8_t *,uint8_t *>,int64_t> * frameBuffers;
-  };
-#endif
+  // already declared in CameraInterface:
+  //std::pair<std::pair<uint8_t *,uint8_t *>,int64_t> frameBuffers[numBuffers];
 
 private:
 #ifdef WITH_REALSENSE
-  rs::device *dev;
-  rs::context ctx;
-
-  RGBCallback * rgbCallback;
-  DepthCallback * depthCallback;
+  rs2::device dev;
+  rs2::context ctx;
+  rs2::pipeline pipe;
 #endif
 
   bool initSuccessful;
   std::string errorText;
   
   ThreadMutexObject<int> latestRgbIndex;
-  std::pair<uint8_t *,int64_t> rgbBuffers[numBuffers];
+
+  // already declared in CameraInterface:
+  //ThreadMutexObject<int> latestDepthIndex;
 
   int64_t lastRgbTime;
   int64_t lastDepthTime;
